e8b36e8e976f5c8dd34a8e39840d6953c04eb157
zaelgohary <zainabelgohary19@gmail.com>
2024-08-21 13:08:27 +0300

    Edit readme, apply migrations, lint admin & models

--- sms/admin.py
+++ sms/admin.py
@@ -2,128 +2,192 @@ from django.contrib import admin
 
 from .models import *
 
-class BaseAdmin(admin.ModelAdmin):    
+
+class BaseAdmin(admin.ModelAdmin):
     def get_list_display(self, _):
-        return self.list_display + ('service_system', 'created_at', 'updated_at')
+        return self.list_display + ("service_system", "created_at", "updated_at")
+
 
 @admin.register(EnvironmentVariable)
 class EnvironmentVariableAdmin(BaseAdmin):
-  list_display = ('name', 'value')
+    list_display = ("name", "value")
+
 
 @admin.register(ConfigurationFile)
-class ConfigurationFileAdmin(admin.ModelAdmin):
-  list_display = ('file_path', 'description')
+class ConfigurationFileAdmin(BaseAdmin):
+    list_display = ("file_path", "description")
+
 
 @admin.register(Dependency)
-class DependencyAdmin(admin.ModelAdmin):
-  list_display = ('name', 'version')
+class DependencyAdmin(BaseAdmin):
+    list_display = ("name", "version")
+
 
 @admin.register(NetworkConfiguration)
-class NetworkConfigurationAdmin(admin.ModelAdmin):
-  list_display = ('dns', 'gateway', 'subnet')
+class NetworkConfigurationAdmin(BaseAdmin):
+    list_display = ("dns", "gateway", "subnet")
+
 
 @admin.register(Application)
-class ApplicationAdmin(admin.ModelAdmin):
-  list_display = ('name', 'version','description')
+class ApplicationAdmin(BaseAdmin):
+    list_display = ("name", "version", "description")
+
 
 @admin.register(Port)
-class PortAdmin(admin.ModelAdmin):
-  list_display = ('application', 'port_number', 'protocol')
+class PortAdmin(BaseAdmin):
+    list_display = ("application", "port_number", "protocol")
+
 
 @admin.register(LoggingConfiguration)
-class LoggingConfigurationAdmin(admin.ModelAdmin):
-  list_display = ('log_file_path', 'log_level')
+class LoggingConfigurationAdmin(BaseAdmin):
+    list_display = ("log_file_path", "log_level")
+
 
 @admin.register(MonitoringTool)
-class MonitoringToolAdmin(admin.ModelAdmin):
-  list_display = ('name', 'version')
+class MonitoringToolAdmin(BaseAdmin):
+    list_display = ("name", "version")
+
 
 @admin.register(HealthCheck)
-class HealthCheckAdmin(admin.ModelAdmin):
-  list_display = ('endpoint', 'criteria')
+class HealthCheckAdmin(BaseAdmin):
+    list_display = ("endpoint", "criteria")
+
 
 @admin.register(Containerization)
-class ContainerizationAdmin(admin.ModelAdmin):
-  list_display = ('container_runtime', 'orchestration_tool', 'image_info')
+class ContainerizationAdmin(BaseAdmin):
+    list_display = ("container_runtime", "orchestration_tool", "image_info")
+
 
 @admin.register(DeploymentTool)
-class DeploymentToolAdmin(admin.ModelAdmin):
-  list_display = ('name', 'version', 'info')
+class DeploymentToolAdmin(BaseAdmin):
+    list_display = ("name", "version", "info")
+
 
 @admin.register(ScalingConfiguration)
-class ScalingConfigurationAdmin(admin.ModelAdmin):
-  list_display = ('scaling_policy', 'auto_scaling_trigger')
+class ScalingConfigurationAdmin(BaseAdmin):
+    list_display = ("scaling_policy", "auto_scaling_trigger")
+
 
 @admin.register(BackupConfiguration)
-class BackupConfigurationAdmin(admin.ModelAdmin):
-  list_display = ('backup_schedule', 'backup_location')
+class BackupConfigurationAdmin(BaseAdmin):
+    list_display = ("backup_schedule", "backup_location")
+
 
 @admin.register(UserPermission)
-class UserPermissionAdmin(admin.ModelAdmin):
-  list_display = ('user', 'permissions')
+class UserPermissionAdmin(BaseAdmin):
+    list_display = ("user", "permissions")
+
 
 @admin.register(DisasterRecovery)
-class DisasterRecoveryAdmin(admin.ModelAdmin):
-  list_display = ('recovery_plan', 'backup_locations', 'testing_schedule')
+class DisasterRecoveryAdmin(BaseAdmin):
+    list_display = ("recovery_plan", "backup_locations", "testing_schedule")
+
 
 @admin.register(Runbook)
-class RunbookAdmin(admin.ModelAdmin):
-  list_display = ('run_command', 'verify_command', 'upgrade_command', 'rollback_command')
+class RunbookAdmin(BaseAdmin):
+    list_display = (
+        "run_command",
+        "verify_command",
+        "upgrade_command",
+        "rollback_command",
+    )
 
 
 class BaseAdminInline(admin.StackedInline):
     extra = 1
 
+
 class EnvironmentVariableInline(BaseAdminInline):
-  model = EnvironmentVariable
+    model = EnvironmentVariable
+
 
 class ConfigurationFileInline(BaseAdminInline):
-  model = ConfigurationFile
+    model = ConfigurationFile
+
 
 class DependencyInline(BaseAdminInline):
-  model = Dependency
+    model = Dependency
+
 
 class NetworjConfiguartionInline(BaseAdminInline):
-  model = NetworkConfiguration
+    model = NetworkConfiguration
+
 
 class ApplicationInline(BaseAdminInline):
-  model = Application
+    model = Application
+
 
 class PortInline(BaseAdminInline):
-  model = Port
+    model = Port
+
 
 class LoggingConfigurationInline(BaseAdminInline):
-  model = LoggingConfiguration
+    model = LoggingConfiguration
+
 
 class MonitoringToolInline(BaseAdminInline):
-  model = MonitoringTool
+    model = MonitoringTool
+
 
 class HealthCheckInline(BaseAdminInline):
-  model = HealthCheck
+    model = HealthCheck
+
 
 class ContainerizationInline(BaseAdminInline):
-  model = Containerization
+    model = Containerization
+
 
 class DeploymentToolInline(BaseAdminInline):
     model = DeploymentTool
 
+
 class ScalingConfigurationInline(BaseAdminInline):
-  model = ScalingConfiguration
+    model = ScalingConfiguration
+
 
 class BackupConfigurationInline(BaseAdminInline):
-  model = BackupConfiguration
+    model = BackupConfiguration
+
 
 class UserPermissionInline(BaseAdminInline):
-  model = UserPermission
+    model = UserPermission
+
 
 class DisasterRecoveryInline(BaseAdminInline):
-  model = DisasterRecovery
+    model = DisasterRecovery
+
 
 class RunbookInline(BaseAdminInline):
-  model = Runbook
+    model = Runbook
+
 
 @admin.register(ServiceSystem)
 class ServiceSystemAdmin(admin.ModelAdmin):
-  list_display = ('name', 'location', 'hostname', 'ip_address', 'operating_system', 'created_at', 'updated_at')
-  search_fields = ('name', 'location', 'hostname', 'ip_address', 'operating_system')
-  inlines = [EnvironmentVariableInline, ConfigurationFileInline, DependencyInline, NetworjConfiguartionInline, ApplicationInline, PortInline, LoggingConfigurationInline, MonitoringToolInline, HealthCheckInline, ContainerizationInline, DeploymentToolInline, ScalingConfigurationInline, BackupConfigurationInline, UserPermissionInline, DisasterRecoveryInline, RunbookInline]
+    list_display = (
+        "name",
+        "location",
+        "hostname",
+        "ip_address",
+        "operating_system",
+        "created_at",
+        "updated_at",
+    )
+    search_fields = ("name", "location", "hostname", "ip_address", "operating_system")
+    inlines = [
+        EnvironmentVariableInline,
+        ConfigurationFileInline,
+        DependencyInline,
+        NetworjConfiguartionInline,
+        ApplicationInline,
+        PortInline,
+        LoggingConfigurationInline,
+        MonitoringToolInline,
+        HealthCheckInline,
+        ContainerizationInline,
+        DeploymentToolInline,
+        ScalingConfigurationInline,
+        BackupConfigurationInline,
+        UserPermissionInline,
+        DisasterRecoveryInline,
+        RunbookInline,
+    ]
--- sms/models.py
+++ sms/models.py
@@ -1,171 +1,176 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 
-class TimeStampedModel(models.Model):
-  created_at = models.DateTimeField(auto_now_add=True)
-  updated_at = models.DateTimeField(auto_now=True)
 
-  class Meta:
-    abstract = True
+class TimeStampedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
 
 class ServiceSystem(TimeStampedModel):
-  name = models.CharField(max_length=255, unique=True)
-  description = models.TextField()
-  location = models.CharField(max_length=255)
-  hostname = models.CharField(max_length=255)
-  ip_address = models.GenericIPAddressField()
-  operating_system = models.CharField(max_length=255)
-  cpu_allocation = models.CharField(max_length=255)
-  ram_allocation = models.CharField(max_length=255)
-  disk_allocation = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField()
+    location = models.CharField(max_length=255)
+    hostname = models.CharField(max_length=255)
+    ip_address = models.GenericIPAddressField()
+    operating_system = models.CharField(max_length=255)
+    cpu_allocation = models.CharField(max_length=255)
+    ram_allocation = models.CharField(max_length=255)
+    disk_allocation = models.CharField(max_length=255)
 
-  def __str__(self) -> str:
-    return self.name
-  
+    def __str__(self) -> str:
+        return self.name
+
 
 class BaseModel(TimeStampedModel):
-  service_system = models.ForeignKey(ServiceSystem, on_delete=models.CASCADE)
+    service_system = models.ForeignKey(ServiceSystem, on_delete=models.CASCADE)
 
-  class Meta:
-    abstract = True
+    class Meta:
+        abstract = True
 
 
 class EnvironmentVariable(BaseModel):
-  name = models.CharField(max_length=255, unique=True)
-  value = models.TextField()
+    name = models.CharField(max_length=255, unique=True)
+    value = models.TextField()
 
-  def __str__(self) -> str:
-    return self.name
-  
+    def __str__(self) -> str:
+        return self.name
+
 
 class ConfigurationFile(BaseModel):
-  file_path = models.CharField(max_length=255)
-  description = models.TextField()
+    file_path = models.CharField(max_length=255)
+    description = models.TextField()
 
-  def __str__(self) -> str:
-    return self.file_path
-  
+    def __str__(self) -> str:
+        return self.file_path
+
 
 class Dependency(BaseModel):
-  name = models.CharField(max_length=255, unique=True)
-  version = models.CharField(max_length=50)
+    name = models.CharField(max_length=255, unique=True)
+    version = models.CharField(max_length=50)
 
-  class Meta:
-    verbose_name_plural = "Dependencies"
-  
-  def __str__(self) -> str:
-    return self.name
-  
+    class Meta:
+        verbose_name_plural = "Dependencies"
+
+    def __str__(self) -> str:
+        return self.name
+
 
 class NetworkConfiguration(BaseModel):
-  dns = models.CharField(max_length=255)
-  gateway = models.CharField(max_length=255)
-  subnet = models.CharField(max_length=255)
+    dns = models.CharField(max_length=255)
+    gateway = models.CharField(max_length=255)
+    subnet = models.CharField(max_length=255)
 
-  def __str__(self) -> str:
-    return self.dns
-  
+    def __str__(self) -> str:
+        return self.dns
+
+
 class Application(BaseModel):
-  name = models.CharField(max_length=255, unique=True)
-  version = models.CharField(max_length=50)
-  description = models.TextField()
+    name = models.CharField(max_length=255, unique=True)
+    version = models.CharField(max_length=50)
+    description = models.TextField()
 
-  def __str__(self) -> str:
-    return self.name
-  
+    def __str__(self) -> str:
+        return self.name
+
 
 class Port(BaseModel):
-  application = models.ForeignKey(Application, on_delete=models.CASCADE)
-  port_number = models.IntegerField(validators=[MinValueValidator(1024), MaxValueValidator(65535)])
-  protocol = models.CharField(max_length=50)
+    application = models.ForeignKey(Application, on_delete=models.CASCADE)
+    port_number = models.IntegerField(
+        validators=[MinValueValidator(1024), MaxValueValidator(65535)]
+    )
+    protocol = models.CharField(max_length=50)
 
-  def __str__(self) -> str:
-    return f"{self.application.name}: {self.protocol}/{self.port_number}"
-  
+    def __str__(self) -> str:
+        return f"{self.application.name}: {self.protocol}/{self.port_number}"
+
 
 class LoggingConfiguration(BaseModel):
-  log_file_path = models.CharField(max_length=1024)
-  log_level = models.CharField(max_length=50)
+    log_file_path = models.CharField(max_length=1024)
+    log_level = models.CharField(max_length=50)
 
+    def __str__(self) -> str:
+        return self.log_file_path
 
-  def __str__(self) -> str:
-    return self.log_file_path
-  
 
 class MonitoringTool(BaseModel):
-  name = models.CharField(max_length=255, unique=True)
-  version = models.CharField(max_length=50)
+    name = models.CharField(max_length=255, unique=True)
+    version = models.CharField(max_length=50)
 
-  def __str__(self) -> str:
-    return self.name
-  
+    def __str__(self) -> str:
+        return self.name
+
 
 class HealthCheck(BaseModel):
-  endpoint = models.CharField(max_length=1024)
-  criteria = models.TextField()
+    endpoint = models.CharField(max_length=1024)
+    criteria = models.TextField()
 
-  def __str__(self) -> str:
-    return self.endpoint
-  
+    def __str__(self) -> str:
+        return self.endpoint
+
 
 class Containerization(BaseModel):
-  container_runtime = models.CharField(max_length=255)
-  orchestration_tool = models.CharField(max_length=255)
-  image_info = models.TextField()
+    container_runtime = models.CharField(max_length=255)
+    orchestration_tool = models.CharField(max_length=255)
+    image_info = models.TextField()
 
-  def __str__(self) -> str:
-    return self.container_runtime
-  
+    def __str__(self) -> str:
+        return self.container_runtime
+
+
 class DeploymentTool(BaseModel):
-  name = models.CharField(max_length=255, unique=True)
-  version = models.CharField(max_length=50)
-  info = models.TextField()
+    name = models.CharField(max_length=255, unique=True)
+    version = models.CharField(max_length=50)
+    info = models.TextField()
 
-  def __str__(self) -> str:
-    return self.name
-  
+    def __str__(self) -> str:
+        return self.name
+
 
 class ScalingConfiguration(BaseModel):
-  scaling_policy = models.TextField()
-  auto_scaling_trigger = models.TextField()
+    scaling_policy = models.TextField()
+    auto_scaling_trigger = models.TextField()
 
-  def __str__(self) -> str:
-    return self.scaling_policy
-  
+    def __str__(self) -> str:
+        return self.scaling_policy
+
 
 class BackupConfiguration(BaseModel):
-  backup_schedule = models.TextField()
-  backup_location = models.CharField(max_length=1024)
+    backup_schedule = models.TextField()
+    backup_location = models.CharField(max_length=1024)
 
-  def __str__(self) -> str:
-    return self.backup_location
-  
+    def __str__(self) -> str:
+        return self.backup_location
+
 
 class UserPermission(BaseModel):
-  user = models.CharField(max_length=255)
-  permissions = models.TextField()
+    user = models.CharField(max_length=255)
+    permissions = models.TextField()
 
-  def __str__(self) -> str:
-    return self.user
-  
+    def __str__(self) -> str:
+        return self.user
+
 
 class DisasterRecovery(BaseModel):
-  recovery_plan = models.TextField()
-  backup_locations = models.TextField()
-  testing_schedule = models.TextField()
+    recovery_plan = models.TextField()
+    backup_locations = models.TextField()
+    testing_schedule = models.TextField()
 
-  class Meta:
-    verbose_name_plural = "DisasterRecoveries"
+    class Meta:
+        verbose_name_plural = "DisasterRecoveries"
 
-  def __str__(self) -> str:
-    return self.recovery_plan
-  
+    def __str__(self) -> str:
+        return self.recovery_plan
+
 
 class Runbook(BaseModel):
-  run_command = models.TextField()
-  verify_command = models.TextField()
-  upgrade_command = models.TextField()
-  rollback_command = models.TextField()
+    run_command = models.TextField()
+    verify_command = models.TextField()
+    upgrade_command = models.TextField()
+    rollback_command = models.TextField()
 
-  def __str__(self) -> str:
-    return self.run_command
+    def __str__(self) -> str:
+        return self.run_command
